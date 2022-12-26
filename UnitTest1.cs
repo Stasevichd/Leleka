0bc28f01604771fa7aca2f2d9fc710623a8527ee
Dmytro Stasevich <dg.stasevich@gmail.com>
2022-12-26 21:38:19 +0200

    Start

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,15 +1,36 @@
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+
 namespace Leleka;
-
-public class Tests
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class Tests : PageTest
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
-    }
+
+    //[Test]
+    //public async Task HomePageHaveTitle()
+    //{
+    //    using var playwright = await Playwright.CreateAsync();
+
+    //    await using var browser = await Playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+    //    {
+    //        Headless = true
+    //    });
+    //    var page = await browser.NewPageAsync();
+
+    //    await page.GotoAsync("https://app-leleka-frontend-preprod.azurewebsites.net/");
+
+    //    await Expect(Page).ToHaveTitleAsync("Leleka");
+    //    var loginInput = page.Locator("#Login");
+    //    await loginInput.ClickAsync();
+    //    await loginInput.FillAsync("admin");
+    //    var passwordInput = page.Locator("#Password");
+    //    await passwordInput.ClickAsync();
+    //    await passwordInput.FillAsync("Password");
+
+    //    var loginButton = page.GetByRole(AriaRole.Button, new() { Name = "Login" });
+    //    await loginButton.ClickAsync();
+
+    //}
 }
