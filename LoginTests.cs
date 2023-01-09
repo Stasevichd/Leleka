5653ce477364a3f8a7d80d683409c495ed7e1a2a
Dmytro Stasevich <dg.stasevich@gmail.com>
2023-01-09 13:27:22 +0200

    Refactoring

--- LoginTests.cs
+++ LoginTests.cs
@@ -1,44 +1,62 @@
-﻿using Microsoft.Playwright;
-using Microsoft.Playwright.NUnit;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Leleka.TestsGUI.WebPages;
+using Microsoft.Playwright;
 
 namespace Leleka
 {
-    public class LoginTests : PageTest
+    public class LoginTests
 
     {
-        
+        LoginPage loginpage;
+
+
         [SetUp]
-        public async Task SetUp()
+        public void SetUp()
         {
-            
-           await  Page.GotoAsync("https://app-leleka-frontend-preprod.azurewebsites.net/");
-        }
 
-        [Test]
-        public async Task LoginTest()
-        {
-            var loginIput = Page.Locator("//input[@placeholder='E-mail']");
-            await loginIput.ClickAsync();
-            await loginIput.FillAsync("password@password.com");
-            var passwordinput = Page.Locator("//input[@placeholder='Password']");
-            await passwordinput.ClickAsync();
-            await passwordinput.FillAsync("password@password.com");
-
-            var loginButton = Page.GetByRole(AriaRole.Button, new() { Name = "Login" });
-            await loginButton.ClickAsync();
-            var alert = Page.Locator("//span[contains(text(), 'You have entered an invalid email or password.')]");
-            await Expect(alert).ToBeVisibleAsync();
-
-
-            await Page.WaitForLoadStateAsync();
 
         }
 
+        [TestCase("EMAIL", "Password", ExpectedResult = true)]
+        [TestCase("email@gmail.com", "Password", ExpectedResult = false)]
+
+
+        public async Task<bool> LoginWithInvalidEmail(string login, string password)
+
+        {
+            using var playwright = await Playwright.CreateAsync();
+
+            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = true
+            });
+            var page = await browser.NewPageAsync();
+
+            await page.GotoAsync("https://app-leleka-frontend-preprod.azurewebsites.net/");
+            loginpage = new LoginPage(page);
+
+            await loginpage.FillCredentials(login, password);
+            return await loginpage.IsEmailInvalid();
+        }
+
+        [TestCase("artem.hladysko@itechcraft.com.test","Password1231", ExpectedResult =true )]
+        [TestCase("artem.hladysko@itechcraft.com.test", "1", ExpectedResult = true)]
+        public async Task<bool> VerifyLoginWIthInvalidCredentials(string login, string password)
+        {
+            using var playwright = await Playwright.CreateAsync();
+
+            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false
+            });
+            var page = await browser.NewPageAsync();
+
+            await page.GotoAsync("https://app-leleka-frontend-preprod.azurewebsites.net/");
+            loginpage = new LoginPage(page);
+            await loginpage.FillCredentials(login, password);
+            await loginpage.ClickLoginButton();
+           
+            return await loginpage.IsCredentialInvalid();
+        }
 
     }
 }
---
+++ TestsGUI/WebPages/LoginPage.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Playwright;
+
+namespace Leleka.TestsGUI.WebPages
+{
+    class LoginPage
+    {
+        private readonly IPage _page;
+        public const string _invalidEmailErrorMessage = "Invalid E-mail format";
+        public const string _invaliCredentialsErrorMessage = "You have entered an invalid email or password.";
+        public const string _requiredFieldErrorMessage = "Field is required";
+
+
+        public LoginPage(IPage page)
+        {
+            _page = page;
+        }
+
+        private ILocator _loginInput => _page.GetByPlaceholder("E-mail");
+        private ILocator _passwordInput => _page.GetByPlaceholder("Password");
+        private ILocator _loginButton => _page.GetByRole(AriaRole.Button, new() { NameString = "Login" });
+        private ILocator _invalidCredentialsMessage => _page.GetByText(_invaliCredentialsErrorMessage);
+        private ILocator _IncorrectEmail => _page.GetByText(_invalidEmailErrorMessage);
+
+        public async Task FillCredentials(string login, string password)
+        {
+            await _loginInput.FillAsync(login);
+            await _passwordInput.FillAsync(password);
+        }
+
+        public async Task ClickLoginButton() 
+        { 
+            await _loginButton.ClickAsync();
+            await _page.WaitForTimeoutAsync(1000);
+            await _page.ScreenshotAsync(new()
+            {
+                Path = "screenshot.png",
+            });
+        }
+        public async Task<bool> IsCredentialInvalid() => await _invalidCredentialsMessage.IsVisibleAsync();
+
+        public async Task<bool> IsEmailInvalid() => await _IncorrectEmail.IsVisibleAsync();
+
+        public async Task Login(string login, string password)
+        {
+            await FillCredentials(login, password); 
+            await ClickLoginButton();
+        }
+    }
+   
+}
